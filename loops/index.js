1c0e1e5fd0c38b32adf53c1726feb84790faced8
Web <web@it.step>
2022-07-12 21:23:43 +0300

    task1

--- loops/index.js
+++ loops/index.js
@@ -19,10 +19,28 @@
 // } while (i !== 5)
 
 
-for (let i = 0; i < 5; i++) {
-    console.log( i );
-    if ( i === 4 ) {
-        continue;
-    }
-    
+// for (let i = 1; i < 6; i++) {
+//     for (let j = 1; j < 5; j++) {
+//     if (j === 3 || i === 3) {
+//     continue;
+//     }
+//     console.log(`${i}.${j}`);
+//     }
+// }
+
+// for (let i = 10; i < 100; i++) {
+//     if (i % 6 !== 0 && i % 4 === 0) {
+//     console.log(i);
+//     }
+// }
+
+
+
+let i = +prompt('Введите число');
+
+for ( ;i === 0; ) {
+    if (i > 0) {
+    console.log(i--);
+    } else {console.log(i++);
+}
 }
