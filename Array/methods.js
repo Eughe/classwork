e6c95b77fd097327181703f5a35c0fe0853ae847
Web <web@it.step>
2022-07-26 18:30:51 +0300

    2607

---
+++ Array/methods.js
@@ -0,0 +1,16 @@
+
+
+// const arr = [23, 4, 56, 43, 0, 54, -23, 34, 23];
+const arr2 = ['one', 'two', 'three'];
+
+// const resultArr = arr.concat(arr2);
+
+// const resultArr = arr2.push(100);
+// const resultArr = arr2.pop();
+// const resultArr = arr2.shift();
+// const resultArr = arr2.unshift();
+
+arr2.indexOf('two');
+
+
+console.log( resultArr );
