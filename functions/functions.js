5d342a08fcf0d7def4b872b4b80669240730651f
Web <web@it.step>
2022-08-02 21:20:49 +0300

    functions 0208

---
+++ functions/functions.js
@@ -0,0 +1,42 @@
+
+// let arr = [24,1,4,4,23,43,23,4,23];
+// let sum = 0;
+// let avg = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         sum += arr[i];
+//         avg++;
+//     }
+// }
+
+
+
+
+// function add (a, b) {
+//     if (typeof a === 'number' && typeof b === 'number') {
+//         console.log(a + b);
+//     } else {
+//         console.log('NOT VALID DATA');
+//     }   
+// }
+
+// add(); // запуск функции
+
+// function add (a, b, c = 0) {
+//     console.log(a + b + c);
+// }
+
+// add(2,4);
+
+// function expression
+// const foo = function () {
+//     console.log('i am foo!');
+// };
+
+// function average (a, b, c, d) {
+//     return  (a + b + c + d ) / 4;    
+// };
+
+// console.log(average(6, 8, 47, 5));
+
+
