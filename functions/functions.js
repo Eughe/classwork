39c440475d360f7fb0ee6f49921265a43a780989
Web <web@it.step>
2022-08-02 20:05:29 +0300

    objects 0208

---
+++ functions/functions.js
@@ -0,0 +1,21 @@
+
+
+
+// let arr = [24,1,4,4,23,43,23,4,23];
+// let sum = 0;
+// let avg = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         sum += arr[i];
+//         avg++;
+//     }
+// }
+
+
+
+
+function add () {
+    console.log(a + b);
+}
+
+add()
