7004f182f2a74d1ce51402e0b075069cc5ed3d65
Web <web@it.step>
2022-09-20 21:29:36 +0300

    promise 2009

--- practice/classes/script.js
+++ practice/classes/script.js
@@ -133,7 +133,115 @@
 // isConnected == false. Если заряд батареек == 0, то метод
 // play вернет сообщение с требованием замены батареек.
 
+class Battery {
+  #energy = 100;
 
+  constructor (type) {
+      this.type = type;
+  }
+
+  consumeEnergy (amount) {
+      if (amount <= this.#energy) {
+          this.#energy -= amount;
+      }       
+  }
+
+  get energy () {
+      return this.#energy;
+  }
+
+  recharge () {
+      this.#energy = 100;
+  }
+}
+
+class Device {
+  #batteries = [];
+
+  constructor (batteryType) {
+      this.batteryType = batteryType;
+  }
+
+  insertBatteries (bt1, bt2) {
+      if (bt1?.type !== this.batteryType || bt2?.type !== this.batteryType) {
+          console.log('battery types are incompatible');
+      } else if (this.#batteries.length) {
+          console.log('batteries are already set');
+      } else {
+          this.#batteries = [bt1, bt2];
+      }
+  }
+
+  removeBatteries () {
+      this.#batteries = [];
+  }
+
+  consumeEnergy (val) {
+      this.#batteries.forEach((bt) => bt.consumeEnergy(val));
+  }
+
+  areBatteriesCharged () {
+      return this.#batteries.every((bt) => bt.energy);
+  }
+
+  hasBatteries () {
+      return Boolean(this.#batteries.length);
+  }
+}
+
+class Gamepad extends Device {
+  #isConnected = false;
+
+  constructor (model, batteryType) {
+      super(batteryType);
+      this.model = model;
+  }
+
+  connectTo (device) {
+      if (this.hasBatteries() && !this.#isConnected) {
+          console.log(`${this.model} has been connected to ${device}`);
+          this.#isConnected = true;
+      } else {
+          console.log(`This ${this.model} doesn't have batteries or is already connected`);
+      }
+  }
+  disconnect () {
+      this.#isConnected = false;
+  }
+
+  play () {
+      if (this.areBatteriesCharged() && this.hasBatteries()) {
+          this.consumeEnergy(10);
+          console.log(`you are playing`);
+      } else {
+          this.#isConnected = false;
+          console.log(`Please change/insert batteries`);
+      }
+  }
+}
+
+const bt1 = new Battery('AA');
+const bt2 = new Battery('AA');
+const xboxGamepad = new  Gamepad('Xbox Gamepad', 'AA');
+
+xboxGamepad.insertBatteries(bt1,bt2);
+xboxGamepad.connectTo('Playstation');
+xboxGamepad.play();
+xboxGamepad.play();
+xboxGamepad.play();
+xboxGamepad.play();
+xboxGamepad.play();
+xboxGamepad.play();
+xboxGamepad.play();
+xboxGamepad.play();
+xboxGamepad.play();
+xboxGamepad.play();
+xboxGamepad.play();
+xboxGamepad.play();
+xboxGamepad.play();
+xboxGamepad.removeBatteries();
+xboxGamepad.play();
+xboxGamepad.play();
 
 
 // Написать конструктор товара, который должен принимать
